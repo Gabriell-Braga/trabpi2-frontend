f5a51bc698f687f345dd64febf28a5b115adc5e3
Guilherme Augusto Fernandes <69806054+guigutox@users.noreply.github.com>
2023-05-10 16:04:23 -0300

    Inicio da configuração firebase

---
+++ assets/js/cgf_firebase.js
@@ -0,0 +1,12 @@
+// Configurações do Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyCRAnhjaBVyOm99RcUb7qP-W-IiHdsvaiI",
+  authDomain: "SEU_DOMINIO.firebaseapp.com",
+  projectId: "391319262679",
+  storageBucket: "SEU_BUCKET.appspot.com",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID"
+};
+
+// Inicializar o Firebase
+firebase.initializeApp(firebaseConfig);
