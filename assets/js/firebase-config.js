5a32724a0b08b7618cf2e121e01c894aaceb6f4b
Guilherme Augusto Fernandes <69806054+guigutox@users.noreply.github.com>
2023-05-11 23:17:24 -0300

    Conexão finalizada
    Conexão finalizada, ocorrem 2 erros porem apos pesquias ele são acusados por não usar Node.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,11 +1,9 @@
+// Importar as bibliotecas do Firebase Firestore
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js";
 
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-analytics.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
-
-
-  const firebaseConfig = {
+// Configurar a conexão com o Firebase
+const firebaseConfig = {
     apiKey: "AIzaSyAgD8XTf6Phg5bInu-D1RxW5nNSdjkZANs",
     authDomain: "medinet-827ad.firebaseapp.com",
     projectId: "medinet-827ad",
@@ -13,14 +11,17 @@
     messagingSenderId: "52310695996",
     appId: "1:52310695996:web:2d71e891702b1aa0e92339",
     measurementId: "G-56M8KYD721"
-  };
+};
 
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  const db = getFirestore(app);
-  const auth = getAuth();
+/*
+// Função para adicionar um documento na coleção "test"
+async function adicionarDocumento() {
+  const docRef = await addDoc(collection(db, "test"), { nome: "João" });
+  console.log("Documento criado com ID:", docRef.id);
+}
 
-  
-
-  export { app, analytics, db, auth };
+// Chamar a função para adicionar um documento
+adicionarDocumento();*/
--- assets/js/firebase-methods.js
+++ assets/js/firebase-methods.js
@@ -1,25 +1,12 @@
-import { app, analytics, db, auth } from "./firebase-config.js";
+import { app, db, auth } from "./firebase-config.js";
 
 //AQUI VAO OS METODOS USANDO FIREBASE
 
 
 
-async function testFirebaseConnection() {
-  try {
-    // Referência para a coleção "test"
-    const testCollection = db.collection('test');
-
-    // Dados para adicionar ao documento
-    const data = {
-      nome: 'João',
-    };
-
-    // Adicionar um novo documento na coleção "test"
-    const docRef = await testCollection.add(data);
-    console.log('Documento criado:', docRef.id);
-  } catch (error) {
-    console.error('Erro ao criar documento:', error);
-  }
+async function adicionarDocumento() {
+  const docRef = await addDoc(collection(db, "test"), { nome: "João" });
+  console.log("Documento criado com ID:", docRef.id);
 }
 
 
